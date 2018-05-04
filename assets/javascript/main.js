232506dfaa3ba92ceadcf4513288d7a5c0c80c99
suarezlui <suarezluis@gmail.com>
2018-05-04 00:40:58 -0500

    updates every second

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,6 +13,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var trainsRef = database.ref("/TrainSchedule/Trains");
 var timeRef = database.ref("/TrainSchedule/Timer");
+var timer;
 var name;
 var destination;
 var firstTrain;
@@ -22,9 +23,9 @@ var newTrain = {
   destination: "",
   firstTrain: "",
   frequency: "",
-  train:true
+  train: true
 };
-
+startTimer();
 $("#submit").on("click", function(event) {
   
   name = $("#nameInput").val();
@@ -36,80 +37,71 @@ $("#submit").on("click", function(event) {
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency,
-    train:'true'
+    
   };
-  if(name != "" && destination != "" && firstTrain != "" && frequency != ""){
-  trainsRef.push(newTrain);}
-
-  
+  if (name != "" && destination != "" && firstTrain != "" && frequency != "") {
+     
+    trainsRef.push(newTrain);
+    
+  }
 });
 
-updateTrains()
-function updateTrains(){
-trainsRef.on("child_added", function(snap, key) {
+updateTrains();
+
+function updateTrains() {
   
-  if(snap.val().train == 'true'){
-  
+  trainsRef.on("child_added", function(snap, key) {
+    $(".delete").off("click");
+    
     $("#table").append(
-    "<tr class='row' id='" +
-      snap.key +
-      "'><td class='name'>" +
-      snap.val().name +
-      "</td><td>" +
-      snap.val().destination +
-      "</td><td>" +
-      snap.val().frequency +
-      "</td><td>" +
-      nextArrival(snap.val().firstTrain, parseInt(snap.val().frequency)) +
-      "</td><td class='time'>" +
-      minutesAway(snap.val().firstTrain, parseInt(snap.val().frequency)) +
-      "</td><td class='delete' id='"+snap.key+"'>x</td></tr>"
-    
-  );
-  $(".delete").on("click", function(){
-    
-    trainsRef.child(snap.key).remove();
-    $("#" + snap.key).remove()
-  })}
-});}
+      "<tr class='row' id='" +
+        snap.key +
+        "'><td class='name'>" +
+        snap.val().name +
+        "</td><td>" +
+        snap.val().destination +
+        "</td><td>" +
+        snap.val().frequency +
+        "</td><td>" +
+        nextArrival(snap.val().firstTrain, parseInt(snap.val().frequency)) +
+        "</td><td class='time'>" +
+        minutesAway(snap.val().firstTrain, parseInt(snap.val().frequency)) +
+        "</td><td class='delete' id='" +
+        snap.key +
+        "'>x</td></tr>"
+    );
+    $(".delete").on("click", function(e) {
+      var child = $(this).attr("id");
+      console.log(child);
+      trainsRef.child(child).remove();
+      $("#" + child).remove();
+    });
+  }); 
+}
 
-$(".clear").on("click", function(){
+$(".clear").on("click", function() {
   trainsRef.remove();
-  $(".row").remove();
-  
-})
+});
 
+// Funtion to start timer
+function startTimer() {
+  timer = setInterval(function() {
+    $(".row").remove();
+    
+    updateTrains();
+    
+  }, 1000);
+}
 
-// to update every second:
-/* var timer = setInterval(function(){
-  timeRef.set({
-    name: "",
-    destination: "",
-    firstTrain: "",
-    frequency: ""
-  })
-  timeRef.remove();
-}, 1000 )
-
-timeRef.on("value", function(){
-  a = $(".time").val();
-  console.log(a)
-} ) */
+//function to stop timer
+function stopTimer(){
+  clearInterval(timer)
+}
 
 // Function to calculate the next train cycle after the current
 // time, the argument first is the first ocurrence and is format is
 // "00:00" in military time, the argument frequency is an integer
 // value of minutes. returns the next ocurrence in a string "00:00 AM"
-
-var timer = setInterval(function(){
-  $(".row").remove();
-  updateTrains();
-  
-  //trainsRef.child("Time").set(Date())
-  //trainsRef.child("Time").remove()
-  
-}, 1000 )
-
 function nextArrival(first, frequency) {
   first = first.split(":");
   var pm = false;
@@ -153,23 +145,31 @@ function nextArrival(first, frequency) {
 // Function to calculate the time until next train cycle from the current
 // time, the argument first is the first ocurrence and is format is
 // "00:00" in military time, the argument frequency is an integer
-// value of minutes. returns the time until the next ocurrence as an 
+// value of minutes. returns the time until the next ocurrence as an
 // integer
-function minutesAway(first, frequency){
+function minutesAway(first, frequency) {
   first = first.split(":");
   var pm = false;
   var now = new Date();
   var next = new Date();
   var nextArrival;
+  now.setSeconds(0);
   now = now.getTime();
   next.setHours(parseInt(first[0]));
   next.setMinutes(parseInt(first[1]));
   next.setSeconds(0);
   var hours;
-  var minutes;
+
   while (next < now) {
     next.setMinutes(next.getMinutes() + parseInt(frequency));
   }
-  return Math.ceil((next - now)/1000/60) +":"+ Math.ceil((next - now)/1000)
+  var seconds = 60 - new Date().getSeconds();
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  var minutes = (next - now) / 1000 / 60 - 1;
+  if (minutes < 0) {
+    minutes = 0;
+  }
+  return minutes + ":" + seconds;
 }
-
